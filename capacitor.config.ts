2e25041721a74efc590ecd9a8e43bed33712f8d4
DESKTOP-IF8GR7I\Lenove <avinash@marsdevs.com>
2024-03-10 16:58:07 +0530

    code setup

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'gift-subscribe',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
